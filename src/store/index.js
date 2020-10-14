14fc4d9bccf3d489a41410fd3b5c3e734e134bfe
gaurav386i <yadavgaurav386@gmail.com>
2020-10-14 21:45:09 +0530

    added actions in store

--- src/store/index.js
+++ src/store/index.js
@@ -27,6 +27,11 @@ const store = createStore({
         .then((result) => commit('updateParts', result.data))
         .catch(console.error);
     },
+    addRobotToCart({ commit, state }, robot) {
+      const cart = [...state.cart, robot];
+      axios.post('/api/cart', cart)
+        .then(() => commit('addRobotToCart', robot));
+    },
   },
 });
 
