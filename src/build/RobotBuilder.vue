06f794d50c77c88587c7fcce3407ce12c40778eb
gaurav386i <yadavgaurav386@gmail.com>
2020-10-15 21:29:00 +0530

    added modules into store

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -102,7 +102,7 @@ export default {
       };
     },
     availableParts() {
-      return this.$store.state.parts;
+      return this.$store.state.robots.cart.parts;
     },
   },
   methods: {
@@ -113,7 +113,8 @@ export default {
        + robot.torso.cost
        + robot.rightArm.cost
        + robot.base.cost;
-      this.$store.dispatch('addRobotToCart', { ...robot, cost });
+      this.$store.dispatch('addRobotToCart', { ...robot, cost })
+        .then(() => this.$router.push('/cart'));
       this.addedToCart = true;
     },
   },
--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,10 @@
 import { createStore } from 'vuex';
-import axios from 'axios';
+
+import robotsModule from './modules/robots';
 
 const store = createStore({
-  state() {
-    return {
-      cart: [],
-      parts: null,
-    };
-  },
-  mutations: {
-    addRobotToCart(state, robot) {
-      state.cart.push(robot);
-    },
-    updateParts(state, parts) {
-      state.parts = parts;
-    },
-  },
-  getters: {
-    cartSaleItems(state) {
-      return state.cart.filter((item) => item.head.onSale);
-    },
-  },
-  actions: {
-    getParts({ commit }) {
-      axios.get('/api/parts')
-        .then((result) => commit('updateParts', result.data))
-        .catch(console.error);
-    },
-    addRobotToCart({ commit, state }, robot) {
-      const cart = [...state.cart, robot];
-      axios.post('/api/cart', cart)
-        .then(() => commit('addRobotToCart', robot));
-    },
+  modules: {
+    robots: robotsModule,
   },
 });
 
